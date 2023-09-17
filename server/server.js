335dcad9d3317aec1e54a751e098f7d11d75c98c
Wei <wlin.li2403@gmail.com>
2023-09-17 00:40:40 -0400

    created server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
-import express from 'express'
-
-
 // setting up the express server
-const app = express
+
+
+//imports express to connect the express server for project
+import express from 'express'
+import giftsRouter from './routes/gifts.js'
+
+// initialize the express app
+const app = express()
+
+// used to serve static files
+// general syntax => app.use(<insert middleware>)
 app.use('/public', express.static('./public'))
 app.use('./scripts', express.static('./public/scripts'))
+app.use('/gifts', giftsRouter)
 
+// define route for the root url
+// helps us check that our server is working and responding correctly
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;"> UnEarthed API</h1>')
+})
+
+// start the server on port
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () =>{
+    console.log(`Sever listening on http=//localhost:${PORT}`)
 })
 
 
